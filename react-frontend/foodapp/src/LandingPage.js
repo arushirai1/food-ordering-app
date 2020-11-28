6ac9cb85f186c4877a0df7a8e8cef8119bfe8c68
Arushi Rai <arushirai1999@gmail.com>
2020-11-28 01:25:21 -0600

    Added multipage routing functionality

--- react-frontend/foodapp/src/LandingPage.js
+++ react-frontend/foodapp/src/LandingPage.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react'
 import App from "./App";
+import {Restaurant} from "./Models"
+import { Form, Button, ButtonGroup, ToggleButton, InputGroup, Card } from 'react-bootstrap';
+
 class RestaurantView extends React.Component {
-    render() {
-      return (<div>
-          <h1> RestaurantView </h1>
-      </div>);
+  constructor(props) {
+    super(props);
+    this.state = {'restaurants':[]}
+  }
+  componentDidMount() {
+    //api call to fetch restaurants
+    this.setState({restaurants : [new Restaurant("PizzaParlor", "West Loop", "Most amazing burnt pizza in the world."),
+      new Restaurant("Pizza Parlor2", "West Loop2", "2nd most amazing burnt pizza in the world."),
+      new Restaurant("Cure Pizza Addiction Restaurant", "West Loop3", "We make sure you end up hating pizza after this.")
+    ]})
+  }
+
+  render() {
+    // learn about cards here https://react-bootstrap.github.io/components/cards/
+    return (<div>
+      <h1> RestaurantView </h1>
+
+      {this.state.restaurants.map((restaurant) => (
+       <Card style={{ width: '18rem', color: 'black' }}>
+              <Card.Img variant="top" src={restaurant.photo_url} />
+              <Card.Body>
+                <Card.Title>{restaurant.name}-{restaurant.location}</Card.Title>
+                <Card.Text>
+                  {restaurant.description}
+                </Card.Text>
+                <Button variant="primary">Start Order</Button>
+              </Card.Body>
+            </Card>
+      ))}
+
+    </div>);
+  }
+}
+
+class Cart extends React.Component {
+  render() {
+    return (<div>
+      <header style={{height:'1%'}} >
+                <Button style={{align:'right'}}>View Current Order</Button>
+      </header>
+    </div>)
   }
 }
 class CustomerLanding extends React.Component {
   render() {
     return (
-        <RestaurantView/>
+        <div>
+           <Cart/>
+          <RestaurantView/>
+        </div>
+
     );
   }
 }
@@ -18,20 +62,20 @@ class CustomerLanding extends React.Component {
 class StaffLanding extends React.Component {
   render() {
     return (<div>
-        <h1> Login </h1>
-        <form>
-          <label>
-            Username:
-            <input type="text" name="username" />
-          </label> <br/>
-          <label>
-            Password:
-            <input type="password" name="password" />
-          </label> <br/>
-            <input type="submit" value="Sign Up" />
+      <h1> Login </h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label> <br/>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label> <br/>
+        <input type="submit" value="Sign Up" />
 
-          <input type="submit" value="Sign In" />
-        </form>
+        <input type="submit" value="Sign In" />
+      </form>
     </div>);
   }
 }
@@ -39,20 +83,20 @@ class StaffLanding extends React.Component {
 class AdminLanding extends React.Component {
   render() {
     return (<div>
-        <h1> Login </h1>
-        <form>
-          <label>
-            Username:
-            <input type="text" name="username" />
-          </label> <br/>
-          <label>
-            Password:
-            <input type="password" name="password" />
-          </label> <br/>
-            <input type="submit" value="Sign Up" />
+      <h1> Login </h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label> <br/>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label> <br/>
+        <input type="submit" value="Sign Up" />
 
-          <input type="submit" value="Sign In" />
-        </form>
+        <input type="submit" value="Sign In" />
+      </form>
     </div>);
   }
 }
---
+++ react-frontend/foodapp/src/Models.js
@@ -0,0 +1,10 @@
+class Restaurant {
+    constructor(name, location, description, photo_url="https://static.wikia.nocookie.net/familyguy/images/8/8d/Pizza_parlor.jpg") {
+        this.name = name
+        this.location = location
+        this.description = description
+        this.photo_url = photo_url
+    }
+}
+
+export {Restaurant}
