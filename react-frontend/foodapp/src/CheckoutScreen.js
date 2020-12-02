5e65d17735dee6528778c6e7f9b09ff26e57a1aa
Arushi Rai <arushirai1999@gmail.com>
2020-12-02 02:40:33 -0600

    Finish payment screen

--- react-frontend/foodapp/src/CheckoutScreen.js
+++ react-frontend/foodapp/src/CheckoutScreen.js
@@ -34,7 +34,7 @@ class Payment extends React.Component {
 class CheckoutView extends React.Component {
     constructor(props) {
         super(props);
-        this.state={'option':'Cash', 'selected_card': null, 'cards':[], 'addresses':[], 'selected_address': null, 'total_price':0}
+        this.state={'option':'Cash', 'selected_card': null, 'cards':[], 'addresses':[], 'selected_address': null, 'total_price':0, 'service':'Pickup'}
 
     }
 
@@ -72,6 +72,9 @@ class CheckoutView extends React.Component {
         }
         return x
     }
+    _changeService(service) {
+        this.setState({'service': service})
+    }
     _renderCreditInput (){
         if(this.state.option === 'Credit Card') {
             console.log("test")
@@ -86,12 +89,38 @@ class CheckoutView extends React.Component {
                     <Form.Group controlId="paymentSelectCard">
                         <Form.Label>Select existing card</Form.Label>
                         <Form.Control as="select">
+                            <option>None</option>
+
                             {this.state.cards.map((card)=>(<option>{this._filterCard(card.card_number)}</option>))}
                         </Form.Control>
                     </Form.Group>
                 </div>
             )
         }
+    }
+        _renderAddress (){
+            if(this.state.service === 'Delivery') {
+                return (<div>
+                        <h1>Address Info</h1>
+                        <Form.Group controlId="address" style={{marginRight:"20px"}}>
+                            <Form.Label>
+                                Enter Address OR
+                            </Form.Label>
+                            <Form.Control key="addLine1" type="text" placeholder="Enter address line 1" />
+                            <Form.Control key="state" type="text" placeholder="Enter state" />
+                            <Form.Control key="zip" type="text" placeholder="Enter zip" />
+
+                        </Form.Group>
+                        <Form.Group controlId="addressSelect">
+                            <Form.Label>Select existing address</Form.Label>
+                            <Form.Control as="select">
+                                <option>None</option>
+                                {this.state.addresses.map((card)=>(<option>{card.address_line1}</option>))}
+                            </Form.Control>
+                        </Form.Group>
+                    </div>
+                )
+            }
     }
     render() {
         return (<div>
@@ -101,7 +130,7 @@ class CheckoutView extends React.Component {
                     <Form.Label>
                         Select Payment Method
                     </Form.Label>
-
+                    <br/>
                     {['radio'].map((type) => (
                         <ButtonGroup style={{marginBottom:"20px"}}>
                             {['Cash', 'Credit Card'].map((method) => (
@@ -111,8 +140,20 @@ class CheckoutView extends React.Component {
                     ))}
                 </Form.Group>
                 { this._renderCreditInput()}
-
-
+                <Form.Group controlId="serviceMethod">
+                    <Form.Label>
+                        How will you receive your order
+                    </Form.Label>
+                    <br/>
+                    {['radio'].map((type) => (
+                        <ButtonGroup style={{marginBottom:"20px"}}>
+                            {['Pickup', 'Delivery', 'Dine In'].map((method) => (
+                                <ToggleButton style={{backgroundColor:'green'}} onClick={this._changeService.bind(this, method)} checked={this.state.service === method} type={type} value={method}>{method}</ToggleButton>
+                            ))}
+                        </ButtonGroup>
+                    ))}
+                </Form.Group>
+            {this._renderAddress()}
 
         </div>)
     }
